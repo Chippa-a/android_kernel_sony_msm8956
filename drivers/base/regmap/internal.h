MotorolaMobilityLLC/kernel-msm

regmap: Provide access to regmap_raw_multi_reg_write API
Regcache sync can call multi reg write to sync to hardware
using regmap_raw_multi_reg_write API. Provide access to
regmap_raw_multi_reg_write API to call from regcache sync.

Change-Id: I2240cd090e7485efb6dd82fd2dd8f8b1fc8a0d85
Signed-off-by: Sudheer Papothi <spapothi@codeaurora.org>

drivers/base/regmap/internal.h
@@ -231,6 +231,10 @@ int regcache_lookup_reg(struct regmap *map, unsigned int reg);
 int _regmap_raw_write(struct regmap *map, unsigned int reg,
 		      const void *val, size_t val_len);
 
+int _regmap_raw_multi_reg_write(struct regmap *map,
+				const struct reg_default *regs,
+				size_t num_regs);
+
 void regmap_async_complete_cb(struct regmap_async *async, int ret);
 
 extern struct regcache_ops regcache_rbtree_ops;

Chippa-a/android_kernel_sony_msm8956

drivers/base/regmap/internal.h
@@ -251,6 +251,10 @@ int regcache_lookup_reg(struct regmap *map, unsigned int reg);
 int _regmap_raw_write(struct regmap *map, unsigned int reg,
 		      const void *val, size_t val_len);
 
+int _regmap_raw_multi_reg_write(struct regmap *map,
+				const struct reg_sequence *regs,
+				size_t num_regs);
+
 void regmap_async_complete_cb(struct regmap_async *async, int ret);
 
 enum regmap_endian regmap_get_val_endian(struct device *dev,