MotorolaMobilityLLC/kernel-msm

usb: dwc3: core: Disable internal clock gating conditionally
Currently USB DWC3 controller's internal clock gating is disabled
unconditionally. In few platform, it is possible to enable internal
clock gating with controller. Hence this change adds support to
disable this functionality conditionally using "snps,disable-clk-gating"
device tree property. With this change USB controller's internal clock
gating is enabled by default.

CRs-Fixed: 851877
Change-Id: I17d43a23d3bff0cb516b952c35c4a13af53f7777
Signed-off-by: Mayank Rana <mrana@codeaurora.org>

drivers/usb/dwc3/core.c
@@ -565,7 +565,10 @@ int dwc3_core_init(struct dwc3 *dwc)
 	 * stuck when entering low power modes. Revisit when there is
 	 * a way to differentiate HW that no longer needs this.
 	 */
-	reg |= DWC3_GCTL_DSBLCLKGTNG;
+	if (dwc->disable_clk_gating) {
+		dev_dbg(dwc->dev, "Disabling controller clock gating.\n");
+		reg |= DWC3_GCTL_DSBLCLKGTNG;
+	}
 
 	dwc3_writel(dwc->regs, DWC3_GCTL, reg);
 
@@ -870,6 +873,8 @@ static int dwc3_probe(struct platform_device *pdev)
 		if (!ret)
 			dwc->lpm_nyet_thresh = (u8)lpm_nyet_thresh;
 
+		dwc->disable_clk_gating = of_property_read_bool(node,
+					"snps,disable-clk-gating");
 		if (dwc->enable_bus_suspend) {
 			pm_runtime_set_autosuspend_delay(dev, 500);
 			pm_runtime_use_autosuspend(dev);

Chippa-a/android_kernel_sony_msm8956

Signed-off-by: Jack Pham <jackp@codeaurora.org>

drivers/usb/dwc3/core.c
@@ -1258,6 +1258,8 @@ static int dwc3_probe(struct platform_device *pdev)
 				    &dwc->hsphy_interface);
 	device_property_read_u32(dev, "snps,quirk-frame-length-adjustment",
 				 &dwc->fladj);
+	dwc->disable_clk_gating = device_property_read_bool(dev,
+				"snps,disable-clk-gating");
 
 	if (dwc->enable_bus_suspend) {
 		pm_runtime_set_autosuspend_delay(dev, 500);