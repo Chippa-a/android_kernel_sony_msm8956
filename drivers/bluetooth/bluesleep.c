bb59d562e7c4c13fbf0d60b3c98f2242fd9f8334
Angelo G. Del Regno <kholk11@gmail.com>
2017-05-15 13:16:30 +0200

    bluetooth: bluesleep: Remove IRQF_DISABLED noop flag
    This flag has been a NOOP since kernel 2.6.36.
    
    Change-Id: Icff2485962f20af38bd8ae20a16b9f273c8c4063

--- drivers/bluetooth/bluesleep.c
+++ drivers/bluetooth/bluesleep.c
@@ -567,7 +567,7 @@ static int bluesleep_probe(struct platform_device *pdev)
 			bsi->irq_polarity);
 
 	ret = request_irq(bsi->host_wake_irq, bluesleep_hostwake_isr,
-			IRQF_DISABLED | IRQF_TRIGGER_RISING,
+			IRQF_TRIGGER_RISING,
 			"bluetooth hostwake", NULL);
 	if (ret  < 0) {
 		pr_err("Couldn't acquire BT_HOST_WAKE IRQ");
