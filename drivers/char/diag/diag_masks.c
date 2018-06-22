3f4b1a24a4284bee8f65e52b56e5902add48a7f3
Mohit Aggarwal <maggarwa@codeaurora.org>
2018-06-22 12:05:15 +0530

    diag: Update mask pointers after reallocation
    Currently, mask pointers are not updated in
    case peripherals are supporting more mask
    tables. The patch updates the mask pointers
    properly.
    
    Change-Id: I1360c722076fca0215e0ccd28247c4741a1ebd88
    Signed-off-by: Mohit Aggarwal <maggarwa@codeaurora.org>
    Signed-off-by: Manoj Prabhu B <bmanoj@codeaurora.org>

--- drivers/char/diag/diag_masks.c
+++ drivers/char/diag/diag_masks.c
@@ -709,10 +709,12 @@ static int diag_cmd_set_all_msg_mask(unsigned char *src_buf, int src_len,
 	msg_mask.status = (req->rt_mask) ? DIAG_CTRL_MASK_ALL_ENABLED :
 					   DIAG_CTRL_MASK_ALL_DISABLED;
 	for (i = 0; i < driver->msg_mask_tbl_count; i++, mask++) {
-		mutex_lock(&mask->lock);
-		memset(mask->ptr, req->rt_mask,
-		       mask->range * sizeof(uint32_t));
-		mutex_unlock(&mask->lock);
+		if (mask && mask->ptr) {
+			mutex_lock(&mask->lock);
+			memset(mask->ptr, req->rt_mask,
+			       mask->range * sizeof(uint32_t));
+			mutex_unlock(&mask->lock);
+		}
 	}
 	mutex_unlock(&msg_mask.lock);
 	mutex_unlock(&driver->msg_mask_lock);
@@ -1002,6 +1004,8 @@ static int diag_cmd_set_log_mask(unsigned char *src_buf, int src_len,
 	}
 	mutex_lock(&log_mask.lock);
 	for (i = 0; i < MAX_EQUIP_ID && !status; i++, mask++) {
+		if (!mask || !mask->ptr)
+			continue;
 		if (mask->equip_id != req->equip_id)
 			continue;
 		mutex_lock(&mask->lock);
@@ -1074,9 +1078,11 @@ static int diag_cmd_disable_log_mask(unsigned char *src_buf, int src_len,
 	}
 
 	for (i = 0; i < MAX_EQUIP_ID; i++, mask++) {
-		mutex_lock(&mask->lock);
-		memset(mask->ptr, 0, mask->range);
-		mutex_unlock(&mask->lock);
+		if (mask && mask->ptr) {
+			mutex_lock(&mask->lock);
+			memset(mask->ptr, 0, mask->range);
+			mutex_unlock(&mask->lock);
+		}
 	}
 	log_mask.status = DIAG_CTRL_MASK_ALL_DISABLED;
 	diag_update_userspace_clients(LOG_MASKS_TYPE);
