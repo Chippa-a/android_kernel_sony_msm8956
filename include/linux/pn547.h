99db73cffb6f775cad46874a834a9a35eb52298a
Chippa-a <vusal1372@gmail.com>
2018-09-19 23:10:31 +0300

    misc: Update NFC pn547 driver
    - based on driver from bq kernel
    - cleaned and adapted for us
    
    Change-Id: I901bddee77b27c84903f76252edda2b05a2fea48

--- include/linux/pn547.h
+++ include/linux/pn547.h
@@ -15,29 +15,20 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-/*
- * Copyright (C) 2014 Sony Mobile Communications Inc.
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2, as
- * published by the Free Software Foundation.
- */
 
 #ifndef _PN547_H_
 #define _PN547_H_
 
 #define PN547_DEVICE_NAME "pn547"
 
-enum pn547_init_deinit_cmd {
-	PN547_INIT,
-	PN547_DEINIT,
-};
-
-enum pn547_set_pwr_cmd {
-	PN547_SET_PWR_OFF,
-	PN547_SET_PWR_ON,
-	PN547_SET_PWR_FWDL,
-};
+/*
+ * PN547 power control via ioctl
+ * PN547_SET_PWR(0): power off
+ * PN547_SET_PWR(1): power on
+ * PN547_SET_PWR(2): reset and power on with firmware download enabled
+ */
+#define PN547_MAGIC	0xE9
+#define PN547_SET_PWR	_IOW(PN547_MAGIC, 0x01, unsigned int)
 
 enum pn547_state {
 	PN547_STATE_UNKNOWN,
@@ -47,10 +38,9 @@ enum pn547_state {
 };
 
 struct pn547_i2c_platform_data {
-	int irq_gpio;
-	int fwdl_en_gpio;
-	int ven_gpio;
+	unsigned int irq_gpio;
+	unsigned int ven_gpio;
+	unsigned int firm_gpio;
 };
 
 #endif
-
