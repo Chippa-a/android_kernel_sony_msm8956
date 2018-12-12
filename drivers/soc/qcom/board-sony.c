1fceec348454d033d50fe61124b388fb43e42ea8
Chippa-a <vusal1372@gmail.com>
2018-12-12 22:50:44 +0300

    arm64: Move androidboot.mode=charger hack to board-sony
    Change-Id: I968b011540b9b57966cc9cce6a6346b211276c50

--- drivers/soc/qcom/board-sony.c
+++ drivers/soc/qcom/board-sony.c
@@ -3,11 +3,42 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
+#include <asm/setup.h>
 
 struct proc_info_node {
     const char *filename;
     const struct file_operations fops;
 };
+
+/*
+ * HACK: These two functions sets the androidboot.mode=charger based
+ * on the Sony Mobile parameters startup and warmboot.
+ */
+static unsigned long sony_startup;
+
+static int __init sony_param_startup(char *str)
+{
+	if (kstrtoul(str, 16, &sony_startup))
+		return 1;
+
+	return 0;
+}
+early_param("startup", sony_param_startup);
+
+static int __init sony_param_warmboot(char *str)
+{
+	unsigned long warmboot;
+
+	if (kstrtoul(str, 16, &warmboot))
+		return 1;
+
+	if (!warmboot && (sony_startup & 0x4004))
+		strlcat(boot_command_line, " androidboot.mode=charger",
+			COMMAND_LINE_SIZE);
+
+	return 0;
+}
+early_param("warmboot", sony_param_warmboot);
 
 /*
  * SIM variant information
