4f1c051adc6ba8c7187975b5b11d1022913d8876
Minchan Kim <minchan@google.com>
2018-12-27 11:23:02 +0900

    Revert "mm: make shrink_page_list with pages work from multiple zones"
    This reverts commit 21bb0e1a7b5c2972b5c3fd7da5c67288e477a8fa.
    Bug: 121158602
    
    Change-Id: I11a0c0e37b295ceefbf7bce5ddb18bc3dd5ce758
    Signed-off-by: Minchan Kim <minchan@google.com>

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -826,8 +826,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			goto keep;
 
 		VM_BUG_ON(PageActive(page));
-		if (zone)
-			VM_BUG_ON(page_zone(page) != zone);
+		VM_BUG_ON(page_zone(page) != zone);
 
 		sc->nr_scanned++;
 
@@ -1011,7 +1010,7 @@ static unsigned long shrink_page_list(struct list_head *page_list,
 			 */
 			if (page_is_file_cache(page) &&
 					(!current_is_kswapd() ||
-				(zone && !zone_is_reclaim_dirty(zone)))) {
+					 !zone_is_reclaim_dirty(zone))) {
 				/*
 				 * Immediately reclaim when written back.
 				 * Similar in principal to deactivate_page()
