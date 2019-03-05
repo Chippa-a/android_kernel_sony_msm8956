d3e4fadf6fef01cbbbb1aae1c60512e063b785c9
Chippa-a <bagirovvusal99@gmail.com>
2019-03-05 19:12:58 +0300

    thermal: tsens: Add calibration support for msm8976/56
    Based on previous implementation and adapted
    for the new driver.
    
    Change-Id: I1d16611c02fd223bba38a6b7ffa5de1cc54ff508
    Signed-off-by: Chippa-a <bagirovvusal99@gmail.com>

--- drivers/thermal/tsens.h
+++ drivers/thermal/tsens.h
@@ -23,6 +23,7 @@
 
 #define DEBUG_SIZE				10
 #define TSENS_MAX_SENSORS			16
+#define TSENS_NUM_SENSORS_8976			11
 #define TSENS_NUM_SENSORS_8937			11
 #define TSENS_NUM_SENSORS_8909			5
 #define TSENS_CONTROLLER_ID(n)			(n)
@@ -163,9 +164,10 @@ struct tsens_device {
 };
 
 extern const struct tsens_data data_tsens2xxx, data_tsens23xx, data_tsens24xx;
-extern const struct tsens_data data_tsens14xx, data_tsens1xxx_8909;
+extern const struct tsens_data data_tsens14xx, data_tsens14xx_8976, data_tsens1xxx_8909;
 extern struct list_head tsens_device_list;
 
+extern int calibrate_8976(struct tsens_device *tmdev);
 extern int calibrate_8937(struct tsens_device *tmdev);
 extern int calibrate_8909(struct tsens_device *tmdev);
 
