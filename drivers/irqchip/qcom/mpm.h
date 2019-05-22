83bd8ddcacb015027fc1fb110663bc770315b9dd
Chippa-a <bagirovvusal99@gmail.com>
2019-05-23 00:21:29 +0300

    drivers: irqchip: qcom: Add MSM8976 pin data for MPM
    Add the mpm pin input mapping for msm8976 chip.
    
    Change-Id: I25f2889aa8d2d3f30132487133ea7444527f6b1f
    Signed-off-by: Chippa-a <bagirovvusal99@gmail.com>

--- drivers/irqchip/qcom/mpm.h
+++ drivers/irqchip/qcom/mpm.h
@@ -30,4 +30,7 @@ extern const struct mpm_pin mpm_msm8937_gpio_chip_data[];
 extern const struct mpm_pin mpm_msm8909_gic_chip_data[];
 extern const struct mpm_pin mpm_msm8909_gpio_chip_data[];
 
+extern const struct mpm_pin mpm_msm8976_gic_chip_data[];
+extern const struct mpm_pin mpm_msm8976_gpio_chip_data[];
+
 #endif /* __QCOM_MPM_H__ */
